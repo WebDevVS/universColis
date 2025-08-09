f41559de27576ef16c62e6f7c63f87e4fe61c01a
VS Web <web.world.vs@gmail.com>
2025-08-09 18:05:55 +0200

    Correction

--- config/express.js
+++ config/express.js
@@ -64,7 +64,7 @@ module.exports = (app) => {
         // Seules les sources JS autorisées (aucun <script> inline ici !)
         scriptSrc: [
           "'self'",
-          "'sha256-8/jB750iB8ST9rcjq6lq1mA063aJk138rGhQRe4zG5M='",
+          "'sha256-sMzAKC7O0yoV3dI2BGeifm/ktjbdlPXtQUoBQ3Tm3+I='",
           "https://www.googletagmanager.com",
           "https://www.google-analytics.com",
           "https://www.17track.net",
