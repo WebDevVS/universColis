0a1089a9508f503371ad2b8c903e69da934918dc
VS Web <web.world.vs@gmail.com>
2025-08-30 17:14:02 +0200

    Correction

--- config/routes.js
+++ config/routes.js
@@ -10,7 +10,6 @@ const aboutUsController = require("../controllers/aboutUsController")
 const commentCaMarcheController = require("../controllers/commentCaMarcheController")
 const mentionsLegalesController = require("../controllers/mentionsLegalesController")
 const confidentialiteController = require("../controllers/confidentialiteController")
-const actualitesController = require("../controllers/actualitesController") 
 
 const cspSuivi = require("../middlewares/cspSuivi");
 
@@ -25,7 +24,6 @@ module.exports = (app) => {
     app.use('/comment-ca-marche', commentCaMarcheController)
     app.use('/mentions-legales', mentionsLegalesController)
     app.use('/politique-confidentialite', confidentialiteController)
-    app.use('/actualites', actualitesController)
 
     app.use((req, res) => {
         res.status(404).render('404');
