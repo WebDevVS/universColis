217ae04576b86fcb37b4e429a91b7ac74e4c0bf7
VS Web <web.world.vs@gmail.com>
2025-12-19 15:05:03 +0100

    Correction

--- services/eurosenderService.js
+++ services/eurosenderService.js
@@ -498,6 +498,8 @@ async function getEurosenderOffers(searchContext, boxtalData) {
             timeout: 15000
         });
 
+        console.log(JSON.stringify(res.data, null, 2));
+
         const data = res.data || {};
         const options = data.options || {};
         const serviceTypes = Array.isArray(options.serviceTypes) ? options.serviceTypes : [];
