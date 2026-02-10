1617daed8214df416f529015fb1cd2e98fbfcfd7
VS Web <web.world.vs@gmail.com>
2026-02-10 15:13:53 +0100

    Correction affichage img

--- static/js/glightbox-init.js
+++ static/js/glightbox-init.js
@@ -1,13 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-  if (typeof GLightbox !== 'function') return;
-  GLightbox({
-    selector: '.glightbox',
-    touchNavigation: true,
-    loop: false,
-    autoplayVideos: false,
-    zoomable: true,
-    draggable: true,
-    closeButton: true,
-    closeOnOutsideClick: true
-  });
-});
+(function(){
+  function init(){
+    if (typeof GLightbox !== 'function') return;
+    GLightbox({
+      selector: '.glightbox',
+      touchNavigation: true,
+      loop: false,
+      autoplayVideos: false,
+      zoomable: true,
+      draggable: true,
+      closeButton: true,
+      closeOnOutsideClick: true
+    });
+  }
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', init);
+  } else {
+    init();
+  }
+})();
