6ed835d6cc68f99e950954d819d5a9848cf36a3d
VS Web <web.world.vs@gmail.com>
2026-02-26 14:55:26 +0100

    Correction changement de cookies

--- scripts/minify.js
+++ scripts/minify.js
@@ -5,7 +5,7 @@ const CleanCSS = require('clean-css');
 
 const jsDir = path.join(__dirname, '../static/js');
 const cssDir = path.join(__dirname, '../static/css');
-const cookiesBannerDir = path.join(__dirname, '../static/cookiesBanner');
+
 
 // Minify JS
 fs.readdirSync(jsDir).forEach(file => {
@@ -19,17 +19,7 @@ fs.readdirSync(jsDir).forEach(file => {
   }
 });
 
-// Minify JS in cookiesBanner
-fs.readdirSync(cookiesBannerDir).forEach(file => {
-  if (file.endsWith('.js') && !file.endsWith('.min.js')) {
-    const filePath = path.join(cookiesBannerDir, file);
-    const code = fs.readFileSync(filePath, 'utf8');
-    minify(code, { compress: true, mangle: true }).then(result => {
-      const outPath = filePath.replace('.js', '.min.js');
-      fs.writeFileSync(outPath, result.code, 'utf8');
-    });
-  }
-});
+
 
 // Minify CSS
 fs.readdirSync(cssDir).forEach(file => {
@@ -42,16 +32,7 @@ fs.readdirSync(cssDir).forEach(file => {
   }
 });
 
-// Minify CSS in cookiesBanner
-fs.readdirSync(cookiesBannerDir).forEach(file => {
-  if (file.endsWith('.css') && !file.endsWith('.min.css')) {
-    const filePath = path.join(cookiesBannerDir, file);
-    const code = fs.readFileSync(filePath, 'utf8');
-    const output = new CleanCSS().minify(code);
-    const outPath = filePath.replace('.css', '.min.css');
-    fs.writeFileSync(outPath, output.styles, 'utf8');
-  }
-});
+
 
 // Minify CSS in /purged
 const purgedDir = path.join(__dirname, '../static/css/purged');
